c1fa4a26f2700b62de0d6173b69b578dda818522
Narongrit Sitthisuanchit <ear12ff@gmail.com>
2026-05-30 17:35:06 +0700

    Delete ISC website directory

--- ISC website/tools/fix-activities.ps1
+++
@@ -1,8 +0,0 @@
-$content = Get-Content 'e:\ISC website\index.html' -Raw -Encoding UTF8
-$lines = $content -split "`n"
-# Show lines 400-410 (0-indexed, which is 401-411 in 1-indexed)
-for ($i = 400; $i -lt 412; $i++) {
-    $lineNum = $i + 1
-    $line = $lines[$i].TrimEnd("`r")
-    Write-Host "${lineNum}: $line"
-}
